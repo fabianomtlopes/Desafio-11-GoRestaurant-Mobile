56db255732de2f0856a4af91885f2cb2e508e9a2
Fabiano de Matos Lopes <fabianomtlopes@yahoo.com.br>
2020-06-25 21:19:40 -0300

    Desafio 11 v.1

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -33,6 +33,14 @@ const Orders: React.FC = () => {
   useEffect(() => {
     async function loadOrders(): Promise<void> {
       // Load orders from API
+      const response = await api.get('/orders');
+
+      const data = response.data.map((foods: Food) => ({
+        ...foods,
+        formattedValue: formatValue(foods.price),
+      }));
+
+      setOrders(data);
     }
 
     loadOrders();
